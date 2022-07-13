4af8e6e8c4e1cd760ab69450b7696cca6a7179f3
milicolazo <milirita.colazovinovo@gmail.com>
2022-07-13 09:02:47 -0300

    Defensive programming and tests

--- inflammation/models.py
+++ inflammation/models.py
@@ -53,6 +53,8 @@ def patient_normalise(data):
 
     Negative values are rounded to 0.
     """
+    if np.any(data < 0):
+        raise ValueError('Inflammation values should not be negative')
     max_data = np.nanmax(data, axis=1)
     with np.errstate(invalid='ignore', divide='ignore'):
         normalised = data / max_data[:, np.newaxis]
--- tests/test_models.py
+++ tests/test_models.py
@@ -48,6 +48,31 @@ def test_daily_min_string():
         error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
 
 
+
+@pytest.mark.parametrize(
+    "test, expected, expect_raises",
+    [
+        (
+            [[-1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            ValueError,
+        ),
+        (
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            None,
+        ),
+    ])
+def test_patient_normalise(test, expected, expect_raises):
+    """Test normalisation works for arrays of one and positive integers."""
+    from inflammation.models import patient_normalise
+    if expect_raises is not None:
+        with pytest.raises(expect_raises):
+            npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
+    else:
+        npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
+
+
 @pytest.mark.parametrize(
     "test, expected",
     [
@@ -55,11 +80,13 @@ def test_daily_min_string():
         ([[1, 1, 1], [1, 1, 1], [1, 1, 1]], [[1, 1, 1], [1, 1, 1], [1, 1, 1]]),
         ([[float('nan'), 1, 1], [1, 1, 1], [1, 1, 1]], [[0, 1, 1], [1, 1, 1], [1, 1, 1]]),
         ([[-1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]]),
-        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]])
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]]),
     ])
 def test_patient_normalise(test, expected):
     """Test normalisation works for arrays of one and positive integers.
        Assumption that test accuracy of two decimal places is sufficient."""
     from inflammation.models import patient_normalise
-    npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
-
+    def test_patient_normalise(test, expected):
+        """Test normalisation works for arrays of one and positive integers."""
+        from inflammation.models import patient_normalise
+        npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
